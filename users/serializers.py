fdb08ee12f1b02e0ce637754ffd1a93f15d87f3c
Joabson Arley <joabsonlg918@gmail.com>
2022-04-07 12:26:26 -0300

    adjust identation

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
 from djoser.serializers import UserCreateSerializer
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
